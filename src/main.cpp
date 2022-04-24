99da972fcb77260a0ea868b560c5fe7e508b949d
vidyagirl <67607376+vidyagirl@users.noreply.github.com>
2022-04-24 20:00:51 -0300

    GLAD load check and window resize callback
    Also added width and height variables.

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,40 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+int width = 1280;
+int height = 720;
+
+void framebufferResizeCallback(GLFWwindow* pWindow, int pWidth, int pHeight) {
+    width = pWidth;
+    height = pHeight;
+    glViewport(0, 0, width, height);
+}
+
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 
     glfwInit();
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Test", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Test", nullptr, nullptr);
+
     if (!window) {
-        std::cout << "Cannot create window!\n";
+        std::cout << "Cannot create window!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
-    gladLoadGL();
+    if (!gladLoadGL()) {
+        std::cout << "Could not initialize GLAD!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+
+    glViewport(0, 0, width, height);
+
+    glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
 
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
@@ -26,4 +45,7 @@ int main(int, char**) {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glfwTerminate();
+    return 0;
 }
